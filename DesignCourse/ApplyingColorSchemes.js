eab751cbc27e705ca979a776af581f044f4f3a21
Jorge Jaramillo <jajaramilloz@gmail.com>
2020-02-11 14:59:50 -0500

    acs

--- DesignCourse/ApplyingColorSchemes.js
+++ DesignCourse/ApplyingColorSchemes.js
@@ -1,1 +1,5 @@
-//www.Coolors
+// https://coolors.co/
+// Generación de patrones de colores:
+// a) de manera aleatoria
+// b) a partir de una imagen
+// Permite exportar a PDF, PNG, JPG, SCSS, link, SVG
